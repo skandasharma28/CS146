12237e9d2a32ae4ee568c41a4a5ef649ab57b6a9
skandasharma28 <154932988+skandasharma28@users.noreply.github.com>
2024-03-19 22:37:22 -0700

    Add files via upload

---
+++ HW9/lowestCommonAncestorClass.java
@@ -0,0 +1,24 @@
+package Hw9;
+
+public class lowestCommonAncestorClass
+{
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        
+        else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        else {
+            return root;
+        }
+    }
+	
+
+}
