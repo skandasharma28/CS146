c053ff5a4e3a217fc6e389073a769cd4ed1c2ff9
skandasharma28 <154932988+skandasharma28@users.noreply.github.com>
2024-03-19 22:38:15 -0700

    Update main.py

--- HW9/main.py
+++ HW9/main.py
@@ -1,1 +1,14 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+            
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+            
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+            
+        else:
+            return root
 
