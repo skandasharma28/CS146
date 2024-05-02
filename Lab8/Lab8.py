fd9f927f06f62eebb17e485ffa064da68bfe451c
skandasharma28 <154932988+skandasharma28@users.noreply.github.com>
2024-05-01 23:17:23 -0700

    Update Lab8.py

--- Lab8/Lab8.py
+++ Lab8/Lab8.py
@@ -10,13 +10,3 @@ class Solution:
                 Tabulation2d[i] = min(Tabulation2d[i], Tabulation2d[i - coin] + 1)
 
         return Tabulation2d[amount] if Tabulation2d[amount] <= amount else -1
-
-# Tester
-def main():
-    solution = Solution()
-    coins = [1,10,25,50,100]
-    amount = 40
-    print("Minimum number of coins needed:", solution.coinChange(coins, amount))  # Output: 3
-
-if __name__ == "__main__":
-    main()
